74f42e89304dffe37cfab1cc2bef3aa297c4ea10
white <white@pfel.ru>
2020-10-05 19:42:31 +0300

    Теперь могёт в чиселки из ссылки

--- urldb/Urldb.py
+++ urldb/Urldb.py
@@ -2,25 +2,40 @@ import re
 import zlib
 from urllib import parse
 import ujson
+import pyperclip
 
 
 class UrlDB:
 	"""
-	Класс урл'ьной базы данных. Он классный.
+	Класс "базы данных", которая на самом деле является просто альтернативным представлением данных.
+
+	*Аргументы*
+	:param base: начальная часть URL, на которую будут накладываться данные
+	:type base: ``str``, optional
+	:param setdb: можно уже имеющуюся строку прочитать и сделать из неё понятное и редактируемое хранилище
+	:type setdb: ``str``, optional
+	:param compress: добавить ли сжатие данных в URL с помощью zlib
+	:type compress: ``bool``
 	"""
 	def __init__(self, base: str = None, setdb: str = None, compress: bool = None):
 		self.link_data = ""
 		self.data = dict()
 		if setdb:
-			if "#c" in setdb or (compress is None or compress is True):
-				self.compress = True
+			if "#c" in setdb:
+				if compress is None or compress is True:
+					self.compress = True
+					self._parse_link_to_db(setdb)
+				else:
+					self.compress = False
+					self.data = self._decode_with_decompress(setdb)
 			else:
 				self.compress = False
-			self._parse_link_to_db(setdb)
+				self._parse_link_to_db(setdb)
 		else:
 			self.compress = True if compress else False
 			if base and self._is_base_url(base):
 				self.base = base
+				self.url = self.base
 			else:
 				self.base = "https://urldb.pfel.cc/data"
 				self.url = self.base
@@ -34,7 +49,7 @@ class UrlDB:
 	def _parse_link_to_db(self, to_parse):
 		if len(to_parse.split("?")) > 1:
 			self.base = to_parse.split("?")[0]
-			self.data = self._decode(to_parse.split("?")[-1])
+			self.data = self._decode(to_parse.split("?")[-1].split("#")[0])
 			self._update_link()
 
 	def _encode(self, data):
@@ -49,25 +64,36 @@ class UrlDB:
 		else:
 			return self._decode_without_decompress(to)
 
-	def _encode_without_compress(self, data):
+	def _encode_without_compress(self, to_encode: dict):
 		pd = []
-		for k, v in data.items():
+		for k, v in to_encode.items():
 			pd.append(f"{parse.quote(ujson.dumps(k))}={parse.quote(ujson.dumps(v))}")
 		enc = "&".join(pd)
 		return enc
 
-	def _encode_with_compress(self, data):
-		return parse.quote(zlib.compress(ujson.dumps(data).encode("utf-8")))
+	def _encode_with_compress(self, to_encode: dict):
+		return parse.quote(zlib.compress(ujson.dumps(to_encode).encode("utf-8")))
 
-	def _decode_with_decompress(self, to):
-		return ujson.loads(zlib.decompress(parse.unquote_to_bytes(to)).decode())
+	def _decode_with_decompress(self, to_decode: str):
+		return ujson.loads(zlib.decompress(parse.unquote_to_bytes(to_decode)).decode())
 
-	def _decode_without_decompress(self, to):
+	def _to_value(self, arg):
+		if arg.isdigit():
+			return int(arg)
+		else:
+			try:
+				return ujson.loads(arg)
+			except ValueError as e:
+				return ujson.loads(f'"{arg}"')
+
+	def _decode_without_decompress(self, to_decode: str):
 		data = dict()
-		pd = to.split("&")
+		pd = to_decode.split("&")
 		for i in pd:
 			tmp = i.split("=")
-			data[ujson.loads(parse.unquote(tmp[0]))] = ujson.loads(parse.unquote(tmp[1]))
+			k, v = parse.unquote(tmp[0]), parse.unquote(tmp[1])
+			k, v = self._to_value(k), self._to_value(v)
+			data[k] = v
 		return data
 
 	def _update_link(self):
@@ -76,6 +102,12 @@ class UrlDB:
 			self.url = f"{self.base}?{self.link_data}#{'c' if self.compress else 'n'}"
 		else:
 			self.url = f"{self.base}"
+
+	def to_clipboard(self):
+		"""
+		Втавсляет в буфер обмена строку
+		"""
+		pyperclip.copy(self.url)
 
 	def __setitem__(self, key, item):
 		self.data[key] = item
@@ -95,6 +127,10 @@ class UrlDB:
 		self._update_link()
 
 	def clear(self):
+		"""
+		Очищает данные и возвращает чистый URL
+		:return: URL (``str``)
+		"""
 		self.data.clear()
 		self._update_link()
 		return self.url
